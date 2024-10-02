9a0eb06e47c3a0eed922ee249a273a16b1c13a88
Agrim <agrimsharma2004@gmail.com>
2024-10-02 00:52:53 -0400

    lmao fixed it

--- tests/app_test.py
+++ tests/app_test.py
@@ -12,7 +12,7 @@ TEST_DB = "test.db"
 def client():
     BASE_DIR = Path(__file__).resolve().parent.parent
     app.config["TESTING"] = True
-    app.config["DATABASE"] = BASE_DIR.joinpatlsh(TEST_DB)
+    app.config["DATABASE"] = BASE_DIR.joinpath(TEST_DB)
 
     init_db() # setup
     yield app.test_client() # tests run here
